96b49835b93084ca95159a1f2390a8938ac3543a
kevintrankt <kevin.tran.kt@gmail.com>
2017-03-15 22:24:39 -0700

    Predicts for month of February
    Still need to figure out how to sync for all days

--- src/start.py
+++ src/start.py
@@ -1,14 +1,46 @@
 import csv
-import numpy 
+import numpy as np
 from sklearn.svm import SVR
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 dates = []
 prices = []
 
+
 def get_data(filename):
-    with open(filename,'r') as input:
-        csvFileReader = csv.reader(input)
-        for row in input:
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)
+
+        for row in csvFileReader:
+            if int(row[0].split('-')[0]) > 28:
+                break
             dates.append(int(row[0].split('-')[0]))
-            
+            prices.append(float(row[1]))
+
+    return
+
+
+def predict_prices(dates, prices, x):
+    dates = np.reshape(dates, (len(dates),1))
+
+    svr_lin = SVR(kernel= 'rbf',C=1e3,gamma=0.1)
+
+    svr_lin.fit(dates,prices)
+
+    plt.scatter(dates,prices, color = 'black', label = 'Data')
+    plt.plot(dates,svr_lin.predict(dates),color = 'red', label = 'Linear Model')
+
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Test')
+
+    plt.legend()
+    plt.show()
+
+    return svr_lin.predict(x)[0]
+
+get_data('wdc.csv')
+
+print predict_prices(dates,prices,1)
+
